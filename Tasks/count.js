c1372a09c0528945af51dfcffa1209136e114d9d
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-14 00:33:21 +0200

    Step 2: removed unneeded operations,redeclared variables using 'const' and 'let'

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,12 @@
 // Sum all number values in dict
-'use strict'
+'use strict';
 const count = (obj) => {
   let sum = 0;
-  {
-  }
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
+  for (const key of keys ) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
-  ({});
+  };
   return sum;
 };
-
 module.exports = count;
