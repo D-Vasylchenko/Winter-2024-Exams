164cb9f245a93464e0fbd8ff207a5f40d637148b
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-13 23:26:39 +0200

    Step 2: improved naming, redeclared variables using 'const'

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,14 +1,14 @@
 // Get day number
 'use strict';
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const _parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const parseDay = (dayOfTheWeek) => {
+    let i;
+    for (i = 0; i < days.length; i++) {
+        if (dayOfTheWeek.startsWith(days[i].toLowerCase())) {
+            return i + 1;
+        }
     }
-  }
-  return -1;
+    return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
