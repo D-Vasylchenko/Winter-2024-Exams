0ba1059d3210e5ddaccda4689b94172e07c2f7bd
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-14 00:43:10 +0200

    Step 3: replaced 'for' with 'for...of',removed unneeded operations

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,12 +1,12 @@
 // Sum all number values in dict
 'use strict';
 const count = (obj) => {
-  let sum = 0;
-  const keys = Object.keys(obj);
-  for (const key of keys ) {
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  };
-  return sum;
+    let sum = 0;
+    for (let key of Object.keys(obj)) {
+        const value = obj[key];
+        if (typeof value === 'number') sum += value;
+    }
+
+    return sum;
 };
 module.exports = count;
