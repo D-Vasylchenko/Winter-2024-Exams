65387085847fd743b8b06538c8e03c56c44e0ab8
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-13 23:44:46 +0200

    Step 1: added 'use strict', redeclared variables using 'const'

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,13 +1,13 @@
 // Generate random password
-
-let GeneratePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
-  key = '';
-  for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
-  }
-  return key;
+'use strict';
+const GeneratePassword = (alphabet, length) => {
+    const MAX = alphabet.length;
+    const key = '';
+    for (let i = 0; i < length; i++) {
+        const Index = Math.floor(Math.random() * MAX);
+        key = key + alphabet[Index];
+    }
+    return key;
 };
 
 module.exports = GeneratePassword;
