dc961c3135004c725632c3902d2c6e99b30154c7
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-14 00:06:46 +0200

    Step 3: improved naming

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,13 +1,14 @@
 // Generate random password
 'use strict';
 const GeneratePassword = (alphabet, length) => {
-    const max = alphabet.length;
+    const maxPass = alphabet.length;
     let key = '';
     for (let i = 0; i < length; i++) {
-        const index = Math.floor(Math.random() * max);
+        const index = Math.floor(Math.random() * maxPass);
         key += alphabet[index];
     }
     return key;
 };
 
 module.exports = GeneratePassword;
+
