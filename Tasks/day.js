325bed195e13cd8734e5b6da3f2a654daa9bea07
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-13 23:39:55 +0200

    Step 3: declared a variable in the "for.." loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,8 +2,7 @@
 'use strict';
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const parseDay = (dayOfTheWeek) => {
-    let i;
-    for (i = 0; i < days.length; i++) {
+    for (let i = 0; i < days.length; i++) {
         if (dayOfTheWeek.startsWith(days[i].toLowerCase())) {
             return i + 1;
         }
