1a2f5ecdbdfc97a6d5c0327f7a5730371497e84a
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-14 00:00:55 +0200

    Step 2: replaced 'const' with 'let' in  variable key,replaced addition assignment

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,11 +1,11 @@
 // Generate random password
 'use strict';
 const GeneratePassword = (alphabet, length) => {
-    const MAX = alphabet.length;
-    const key = '';
+    const max = alphabet.length;
+    let key = '';
     for (let i = 0; i < length; i++) {
-        const Index = Math.floor(Math.random() * MAX);
-        key = key + alphabet[Index];
+        const index = Math.floor(Math.random() * max);
+        key += alphabet[index];
     }
     return key;
 };
