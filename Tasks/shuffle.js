5024e703b202794137d1f86ab2c575bf697ddeca
D-Vasylchenko <145137182+D-Vasylchenko@users.noreply.github.com>
2024-01-14 21:59:01 +0200

    Step 3: removed the unnecessary destructuring and block scope.

--- Tasks/shuffle.js
+++ Tasks/shuffle.js
@@ -1,11 +1,8 @@
 // Shuffle an array
 'use strict';
-const shuffle = (...List) => {
-  const [arr] = List;
+const shuffle = (arr) => {
   arr.sort(() => Math.random() - 0.5);
-  {
     return arr;
-  }
 };
 
 module.exports = shuffle;
